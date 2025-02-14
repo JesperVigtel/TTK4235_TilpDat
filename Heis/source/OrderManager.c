788fa1ae2084a9f55231358be6ee275ee65915b7
Andreas <andrfra@stud.ntnu.no>
2025-02-14 11:11:13 +0100

    finished ordermanager.c i think

--- Heis/source/OrderManager.c
+++ Heis/source/OrderManager.c
@@ -1,21 +1,7 @@
 
 #include "OrderManager.h"
 
-/*
-For dynamic array for orderManager
-#include "OrderManager.h"
 
-void initOrderManager(struct OrderManager* om, int maxOrders) {
-    om->orderQueue = (int*)malloc(maxOrders * sizeof(int));
-    om->numOrders = 0;
-}
-
-void freeOrderManager(struct OrderManager* om) {
-    free(om->orderQueue);
-    om->orderQueue = NULL;
-    om->numOrders = 0;
-}
-*/
 
 void addOrder(struct OrderManager* om, int order) {
     om -> orderQueue = realloc(om -> orderQueue, (om -> numOrders + 1) * sizeof(int));
@@ -33,5 +19,24 @@ int getNextOrder(struct OrderManager* om) {
         for (int i = 1; i < om -> numOrders; i++) {
             om -> orderQueue[i -1] = om -> orderQueue[i]; //shift all orders to the left
         }
+        om -> numOrders --;
+        om -> orderQueue = realloc(om -> orderQueue, om -> numOrders * sizeof(int)); //resize the array
+        int nextorder = om -> orderQueue[0];
+        return nextorder;
+    }
+}
+
+void clearOrders(struct OrderManager* om) {
+    free(om -> orderQueue);
+    om -> orderQueue = NULL;
+    om -> numOrders = 0;
+}
+
+bool hasPendingOrders(struct OrderManager* om) {
+    if(om -> numOrders ==0) {
+        return false:
+    }
+    else {
+        return true;
     }
 }
