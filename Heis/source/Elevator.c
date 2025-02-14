b49d721f552c86e673d4ee2ab10958910c29bc06
Jesper Vigtel Hølland <74411753+JesperVigtel@users.noreply.github.com>
2025-02-14 10:17:04 +0100

    order manganger to c file

--- Heis/source/Elevator.c
+++ Heis/source/Elevator.c
@@ -1,18 +1,3 @@
 //Test
 
 //Slem andreas
-/*
-For dynamic array for orderManager
-#include "OrderManager.h"
-
-void initOrderManager(struct OrderManager* om, int maxOrders) {
-    om->orderQueue = (int*)malloc(maxOrders * sizeof(int));
-    om->numOrders = 0;
-}
-
-void freeOrderManager(struct OrderManager* om) {
-    free(om->orderQueue);
-    om->orderQueue = NULL;
-    om->numOrders = 0;
-}
-*/
---
+++ Heis/source/OrderManager.c
@@ -0,0 +1,16 @@
+
+/*
+For dynamic array for orderManager
+#include "OrderManager.h"
+
+void initOrderManager(struct OrderManager* om, int maxOrders) {
+    om->orderQueue = (int*)malloc(maxOrders * sizeof(int));
+    om->numOrders = 0;
+}
+
+void freeOrderManager(struct OrderManager* om) {
+    free(om->orderQueue);
+    om->orderQueue = NULL;
+    om->numOrders = 0;
+}
+*/
