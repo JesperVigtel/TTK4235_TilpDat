4a88f76ed955f41effce6edd00e3fd634793fa3b
Jesper Vigtel Hølland <74411753+JesperVigtel@users.noreply.github.com>
2025-02-14 10:32:30 +0100

    Adding member attributes to the Elevator class

--- Heis/source/Elevator.h
+++ Heis/source/Elevator.h
@@ -1,1 +1,36 @@
-//test
+#ifndef ELEVATOR_H
+#define ELEVATOR_H
+
+#include <stdio.h> //Input/output funcitons
+#include <stdlib.h>
+#include "driver/elevio.h"
+
+
+typedef enum {
+    DIRN_DOWN, DIRN_UP, DIRN_STOP
+} MotorDirection;
+
+typedef enum {
+    IDLE,   //Standig still
+    MOVING, //Mocvign
+    DOOR_OPEN,
+     EMERGENCY_STOPP 
+} ElevatorState;
+
+//Defining the elevator "class"
+struct Elevator = {
+    //Medlsemsvariabler
+    int currentFloor;
+    int targetFloor;
+    MotorDirection motorDir;
+    ElevatorState ElevState;
+
+    //Medlemsfunksjoner
+    void initialize();
+    void moveToFlooe(int targetFloor);
+    void updateStatus();
+    void stop();
+}
+
+
+#endif
