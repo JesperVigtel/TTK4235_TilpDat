dec4039ea3624063a30dd25b2dad336a5a442147
Andreas <andrfra@stud.ntnu.no>
2025-02-28 10:08:09 +0100

    ordermanager

--- Heis/source/OrderControllerfeil.c
+++ Heis/source/OrderControllerfeil.c
@@ -22,3 +22,41 @@ void clearOrders();
 bool hasPendingOrders();
 
 #endif // ORDERMANAGER_H*/
+Elevator elevator;
+
+/*typedef enum {
+    order_none = -1,
+    order_up = BUTTON_HALL_UP,
+    order_down = BUTTON_HALL_DOWN,
+    order_internal = BUTTON_CAB
+} OrderType;
+
+// enumen order_t er en type som kan ha verdiene order_none, order_up, order_down og order_internal og
+// er en type som definerer hvilken type ordre det er snakk om.
+
+typedef struct {
+    OrderType order;
+    int orderFloor;
+} Order;
+
+// structen Order definerer en ordre som består av en order_t og en etasje.
+
+typedef struct {
+    Order *orders;
+    int numOrders;
+} OrderManager;
+
+extern OrderManager orderManager;
+
+// structen OrderManager definerer en ordremanager som består av en peker til en array av Order og en int som
+// holder styr på antall ordre.
+
+void orderManager_init(OrderManager *orderManager);
+void orderManager_addOrder(OrderManager *orderManager, OrderType order, int orderFloor);
+void orderManager_removeOrder(OrderManager *orderManager, OrderType order, int orderFloor);
+void stop_and_clear_all_orders(OrderManager *orderManager);
+void priority_order(OrderManager *orderManager, motorDirection dir, int orderFloor);
+void process_next_order(OrderManager *orderManager, motorDirection dir, int orderFloor);
+bool has_pending_orders(OrderManager *orderManager);
+int getNextOrder();*/
+
--- Heis/source/OrderManager.c
+++ Heis/source/OrderManager.c
@@ -3,3 +3,48 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+
+void addOrder(int floor, int button) {
+    if (floor >= 0 || floor < N_FLOORS && button >= 0 || button < N_BUTTONS) {
+        queue[floor][button] = 1;
+        elevio_buttonLamp(floor, button, 1); //Turn on button Lamp
+    }
+}
+
+void removeOrder(int floor, int button){
+    if (floor >= 0 || floor < N_FLOORS && button >= 0 || button < N_BUTTONS) {
+        queue[floor][button] = 0;
+        elevio_buttonLamp(floor, button, 0); //Turn off button Lamp
+    }
+}
+void clearAllOrders() {
+    for (int i = 0; i < N_FLOORS; i++) {
+        for (int j = 0; j < N_BUTTONS; j++) {
+            queue[i][j] = 0; // clear all orders
+            elevio_buttonLamp(i, j, 0); // Turn off Button Lamp
+        }
+    }
+}
+void printQueue() {
+    printf("\nHeis køsystem:\n");
+    printf("    Opp  Ned  Cabin\n");
+    for (int i = 0; i < FLOORS; i++) {
+        printf("Etasje %d: ", i);
+        for (int j = 0; j < BUTTONS; j++) {
+            printf(" %d ", queue[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void nextOrder(int current_floor, ButtonType Button,) {
+    int current_floor = elevio_floorSensor();
+    
+    
+    return -1;
+}
+
+
+
