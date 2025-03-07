30765866c0d485691df796d7aa415815379ff5ac
Jesper Vigtel Hølland <jesper.hoelland@gmail.com>
2025-03-07 15:44:31 +0100

    clock by smart guy

--- Heis/skeleton_project/source/States.c
+++ Heis/skeleton_project/source/States.c
@@ -51,14 +51,7 @@ void state_doorOpen() {
 
     elevio_motorDirection(DIRN_STOP); 
     elevio_doorOpenLamp(1);
-    nanosleep(&(struct timespec){3, 0}, NULL);
-
-    while (elevio_obstruction()) {
-        panelSignals(); //kan fortsatt ta inn ordre mens obstruksjon
-        if(!elevio_obstruction()){
-            nanosleep(&(struct timespec){3, 0}, NULL);
-        }
-    }
+    waitThreeSeconds();
 
     elevio_doorOpenLamp(0);
     removeFloorOrders(elevio_floorSensor());
@@ -79,12 +72,33 @@ void state_stop() {       //Stanser heisen øyeblikkelig
         elevio_stopLamp(1);
         if (floor != -1) {
             elevio_doorOpenLamp(1); 
+        }
     }
+    elevio_stopLamp(0);
+    if (floor != -1) {
+        waitThreeSeconds();
+    }
+        elevio_doorOpenLamp(0);
+        elevator.state = IDLE; 
 }
-elevio_stopLamp(0);
-if (floor != -1) {
-    nanosleep(&(struct timespec){3, 0}, NULL);
-}
-    elevio_doorOpenLamp(0);
-    elevator.state = IDLE; 
+
+
+void waitThreeSeconds() {
+    struct timespec start, current;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    double elapsed = 0;
+    double wait_time = 3.0;  // Wait time in seconds
+
+    while (elapsed < wait_time) {
+        clock_gettime(CLOCK_MONOTONIC, &current);
+        elapsed = current.tv_sec - start.tv_sec + (current.tv_nsec - start.tv_nsec) / 1e9;
+
+        // Call panelSignals() to check for button presses
+        panelSignals();
+
+        // Check for obstruction
+        if (elevio_obstruction()) {
+            clock_gettime(CLOCK_MONOTONIC, &start);  // Reset the timer if obstruction is detected
+        }
+    }
 }
--- Heis/skeleton_project/source/States.h
+++ Heis/skeleton_project/source/States.h
@@ -2,11 +2,13 @@
 #define STATES_H
 
 #include "Elevator.h"
+#include <time.h>
 
 
 void state_stop();
 void state_doorOpen();
 void state_moving();
 void state_idle();
+void waitThreeSeconds();
 
 #endif
