a22f87c7d6d8eb87cc9d5b03cffa664d98ad05d1
Jesper Vigtel Hølland <jesper.hoelland@gmail.com>
2025-03-13 13:24:00 +0100

    Flere kommentarer

--- Heis/skeleton_project/source/States.h
+++ Heis/skeleton_project/source/States.h
@@ -5,10 +5,10 @@
 #include <time.h>
 
 //Statesene til heisen ved kjøring av program
-void state_stop();
-void state_doorOpen();
-void state_moving();
-void state_idle();
+void state_stop();       //Stanser heisen øyeblikkelig
+void state_doorOpen();   //Dersom heisen skal åpne døra
+void state_moving();     //Dersom heisen er i bevegelse
+void state_idle();       //Dersom heisen er i ro
 
 
 #endif
